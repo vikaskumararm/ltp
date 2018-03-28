a09f7b3bc027f9db767d0067def973619733c4c4
Petr Vorel <pvorel@suse.cz>
2018-03-28 13:28:11 +0200

    network/route: Rewrite route{4,6}-change-gw into new shell API
    * Drop route command (use just ip command), support both IPv4 and IPv6
    * Use unused network range to avoid clash with real network
    * Add verification with ping (previous version sent UDP datagram with
    ns-udpsender, but didn't verify receiving it on rhost and didn't setup
    rhost ip at all)
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

---
+++ testcases/network/stress/route/route-change-gw.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2019 Petr Vorel <pvorel@suse.cz>
+# Copyright (c) International Business Machines Corp., 2006
+# Author: Mitsuru Chinen <mitch@jp.ibm.com>
+
+# Change route gateway
+# lhost: 10.23.x.2, gw (on rhost): 10.23.x.1, rhost: 10.23.0.1
+
+TST_TESTFUNC="test_gw"
+. route-lib.sh
+
+setup()
+{
+	tst_res TINFO "change IPv$TST_IPVER route gateway $NS_TIMES times"
+
+	rt="$(tst_ipaddr_un -p 0 0)"
+	lhost="$(tst_ipaddr_un 1 1)"
+	rhost="$(tst_ipaddr_un 0 1)"
+	tst_add_ipaddr -s -a $lhost
+	tst_add_ipaddr -s -a $rhost rhost
+}
+
+test_gw()
+{
+	local gw="$(tst_ipaddr_un 1 $(($1 + 1)))"
+	local iface="$(tst_iface)"
+
+	tst_res TINFO "testing route over gateway '$gw'"
+
+	tst_add_ipaddr -s -a $gw rhost
+	ROD ip route replace $rt dev $iface via $gw
+	EXPECT_PASS ping$TST_IPV6 -c1 -I $lhost $rhost
+	ROD ip route del $rt dev $iface via $gw
+	tst_del_ipaddr -s -a $gw rhost
+}
+
+tst_run
