2ef4171ab5fe59e147cd62b7bba6b8dc8df997ad
Petr Vorel <pvorel@suse.cz>
2018-06-18 18:53:05 +0200

    net/{stress,virt}: Migrate tests using virt_lib.sh and ipsec_lib.sh
    + these libraries itself.
    
    It was needed to migrate it all in once, as macsec0{1,2}.sh tests are
    using both libraries.
    
    This includes:
    * Create setup, getopts and helper function for both libraries.
    * Using TST_TEST_DATA and TST_TEST_DATA_IFS in many test cases
    (simplify tests and allow to have more TST_CNT to keep readability of
    output + required by udp_ipsec.sh and udp_ipsec_vti.sh otherwise it'd
    need to use getopts to determine type of udp, as passing parameter to
    tst_run is not not recommended to new API).
    * Put code into TST_TESTFUNC functions.
    * Move anything based on TST_IPV6 to TST_SETUP setup functions (as
    TST_IPV6 is not set until setup)
    
    Other changes:
    * udp_ipsec{,_vti}.sh: were calling do_test() twice, with parameter.
    Now they use correctly TST_CNT and no params.
    Order of running tests changed. Before first were run udp tests for all
    items in $IPSEC_SIZE_ARRAY, then all udp_lite tests. Now for each item
    in $IPSEC_SIZE_ARRAY both udp and udp_lite tests are run.
    * gre01.sh: Move back device type setting (changed in 57738337a
    "network/gre01: fix device type setting").
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/network/virt/vxlan01.sh
+++ testcases/network/virt/vxlan01.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
 # Copyright (c) 2014-2015 Oracle and/or its affiliates. All Rights Reserved.
 # Author: Alexey Kodanev <alexey.kodanev@oracle.com>
 #
 # Local test, check if we can create multiple VXLAN interfaces.
 
 TCID=vxlan01
-TST_TOTAL=5
 TST_OPTS="hi:d:"
 TST_PARSE_ARGS=virt_lib_parse_args
+TST_USAGE=virt_lib_usage
 
 virt_type="vxlan"
 start_id=16700000
 
-. virt_lib.sh
-
 options="l2miss l3miss,norsc nolearning noproxy,\
 ttl 0x01 tos 0x01,ttl 0xff tos 0xff,gbp"
+TST_CNT=5
+TST_TESTFUNC=do_test
+. virt_lib.sh
 
-virt_test_01 "$options"
+do_test()
+{
+	virt_test_01 "$options"
+}
 
-tst_exit
+tst_run
