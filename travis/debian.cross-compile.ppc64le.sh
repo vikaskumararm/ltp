a52e716895a2ad0f3e104e31fe894c46b2fd031b
Petr Vorel <pvorel@suse.cz>
2019-01-09 21:53:57 +0100

    net/route: Add netlink based route change gw/dest tests
    * use libmnl and new C API
    * reuse code in tst_net.h (added in previous commit)
    * add shell wrapper route-change-netlink.sh (set environment with
      tst_net.sh instead of using deprecated helpers in testcases/network/stress/ns-tools/)
    * add tst_ipaddr_un() into C API, unlike shell API this implementation
      does not support -c tst_ipaddr_un [-cCOUNTER] [TYPE] syntax.
    
    Travis:
    * add libmnl libraries to most of travis jobs
    
    Cleanup:
    * cleanup test description
    * other cleanup
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- travis/debian.cross-compile.ppc64le.sh
+++ travis/debian.cross-compile.ppc64le.sh
@@ -2,6 +2,12 @@
 # Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
 set -e
 
+dpkg --add-architecture ppc64el
+apt update
+
 apt install -y --no-install-recommends \
 	gcc-powerpc64le-linux-gnu \
-	libc6-dev-ppc64el-cross
+	libc6-dev-ppc64el-cross \
+	libmnl0:ppc64el \
+	libmnl-dev:ppc64el \
+	pkg-config:ppc64el
--- travis/fedora.sh
+++ travis/fedora.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
+# Copyright (c) 2018-2019 Petr Vorel <pvorel@suse.cz>
 set -e
+
+echo "0: $0, $(basename $0)" # FIXME: debug
+
+echo "yum repolist"
+yum repolist
 
 yum -y install \
 	autoconf \
@@ -9,4 +14,6 @@ yum -y install \
 	clang \
 	gcc \
 	findutils \
+	libmnl \
+	pkg-config \
 	redhat-lsb-core
