c0483947a454429b6a8ba7fa2dcc2229eca45d77
Petr Vorel <pvorel@suse.cz>
2019-02-20 12:53:08 +0100

    ima: Add overlay test
    Based on reproducer made by Ignaz Forster <iforster@suse.de>
    used for his not upstreamed patchset [1] and previous report [2].
    
    NOTE: backup variables are needed because ima_setup.sh calling
    tst_mount as well when TMPDIR is on tmpfs device.
    
    [1] https://www.spinics.net/lists/linux-integrity/msg05926.html
    [2] https://www.spinics.net/lists/linux-integrity/msg03593.html
    
    Cc: Mimi Zohar <zohar@linux.vnet.ibm.com>
    Cc: Ignaz Forster <iforster@suse.de>
    Cc: Fabian Vogt <FVogt@suse.com>
    Cc: Amir Goldstein <amir73il@gmail.com>
    Cc: Weihua Du <WHDu@suse.com>
    Tested-by: Ignaz Forster <iforster@suse.de>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

---
+++ testcases/kernel/security/integrity/ima/tests/ima_overlay.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+# Copyright (c) 2019 Petr Vorel <pvorel@suse.cz>
+# Based on reproducer made by Ignaz Forster <iforster@suse.de>
+
+TST_SETUP="setup"
+TST_CLEANUP="cleanup"
+TST_NEEDS_DEVICE=1
+
+. ima_setup.sh
+TST_TESTFUNC="do_test"
+
+setup()
+{
+	lower="$TST_MNTPOINT/lower"
+	upper="$TST_MNTPOINT/upper"
+	work="$TST_MNTPOINT/work"
+	merged="$TST_MNTPOINT/merged"
+	mkdir -p $lower $upper $work $merged
+
+	device_backup="$TST_DEVICE"
+	TST_DEVICE="overlay"
+
+	fs_type_backup="$TST_FS_TYPE"
+	TST_FS_TYPE="overlay"
+
+	mntpoint_backup="$TST_MNTPOINT"
+	TST_MNTPOINT="$merged"
+
+	params_backup="$TST_MNT_PARAMS"
+	TST_MNT_PARAMS="-o lowerdir=$lower,upperdir=$upper,workdir=$work"
+
+	grep -q -e ima_policy= -e ima_appraise_tcb /proc/cmdline || \
+		tst_brk TCONF "Test requires specify IMA policy as kernel parameter"
+}
+
+do_test()
+{
+	local file="foo.txt"
+	local f
+
+	tst_mount
+	mounted=1
+
+	ROD echo lower \> $lower/$file
+	if ! echo overlay > $merged/$file 2>/dev/null; then
+		tst_res TFAIL "Cannot write to merged layer"
+		return
+	fi
+
+	for f in $(find $TST_MNTPOINT -type f); do
+		EXPECT_PASS cat $f \> /dev/null 2\> /dev/null
+	done
+}
+
+cleanup()
+{
+	[ -n "$mounted" ] || return 0
+
+	tst_umount $TST_DEVICE
+
+	TST_DEVICE="$device_backup"
+	TST_FS_TYPE="$fs_type_backup"
+	TST_MNTPOINT="$mntpoint_backup"
+	TST_MNT_PARAMS="$params_backup"
+}
+
+tst_run
