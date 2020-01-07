c003c00c8af94e1a7c24027be4d09a33c523d37b
Petr Vorel <pvorel@suse.cz>
2020-01-07 14:20:32 +0100

    rpc-tirpc: Detect libtirpc with pkg-config
    Using pkg-config is more reliable way than detecting presence of
    libtirpc shared library with headers. As it does not require use $SYSROOT,
    which is not defined everywhere (at least SDKs built in YOCTO as they
    use nonstandard $SDKTARGETSYSROOT, see [1]).
    Fixes: a86d71fe5 ("Implemented autoconf check for libtirpc")
    
    Use only <rpc/*.h> includes, <tirpc/rpc/*.h> removed as not needed
    (pkg-config detects -I/usr/include/tirpc when needed). Therefore rpc.h
    moved out of lapi.
    Fixes: 7fe1a8bf8 ("rpc: Use libtirpc for all RPC tests + detect headers location")
    
    Also remove <netconfig.h> (resp. <tirpc/netconfig.h>) as it's not
    needed.
    Fixes: c0caf8f23 (initial commit of TI-RPC tests)
    
    Use CFLAGS instead of CPPFLAGS in build system (we have C sources).
    
    [1] https://github.com/linux-test-project/ltp/pull/628
    
    Closes: #628
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- travis/debian.cross-compile.ppc64le.sh
+++ travis/debian.cross-compile.ppc64le.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-# Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
+# Copyright (c) 2018-2020 Petr Vorel <pvorel@suse.cz>
 set -e
+
+dpkg --add-architecture ppc64el
+apt update
 
 apt install -y --no-install-recommends \
 	gcc-powerpc64le-linux-gnu \
-	libc6-dev-ppc64el-cross
+	libc6-dev-ppc64el-cross \
+	libtirpc-dev:ppc64el
