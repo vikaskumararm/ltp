vikaskumararm/ltp

commit a86d71fe5dce7b25372199afc9a9bfc5631c84e1

Implemented autoconf check for libtirpc
This library is needed to build testcases/network/rpc/rpc-tirpc.

Signed-off-by: Stanislav Kholmanskikh <stanislav.kholmanskikh@oracle.com>

include/mk/config.mk.in
@@ -44,6 +44,8 @@ LEXLIB			:= @LEXLIB@
 NUMA_CPPFLAGS		:= @NUMA_CPPFLAGS@
 NUMA_LIBS		:= @NUMA_LIBS@
 SELINUX_LIBS		:= @SELINUX_LIBS@
+TIRPC_CPPFLAGS		:= @TIRPC_CPPFLAGS@
+TIRPC_LIBS		:= @TIRPC_LIBS@
 
 prefix			:= @prefix@
 

m4/ltp-tirpc.m4
@@ -1,0 +1,34 @@
+dnl
+dnl Copyright (c) 2014 Oracle and/or its affiliates. All Rights Reserved.
+dnl
+dnl This program is free software; you can redistribute it and/or
+dnl modify it under the terms of the GNU General Public License as
+dnl published by the Free Software Foundation; either version 2 of
+dnl the License, or (at your option) any later version.
+dnl
+dnl This program is distributed in the hope that it would be useful,
+dnl but WITHOUT ANY WARRANTY; without even the implied warranty of
+dnl MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+dnl GNU General Public License for more details.
+dnl
+dnl You should have received a copy of the GNU General Public License
+dnl along with this program; if not, write the Free Software Foundation,
+dnl Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+dnl
+
+dnl
+dnl LTP_CHECK_TIRPC
+dnl ----------------------------
+dnl
+AC_DEFUN([LTP_CHECK_TIRPC],[
+	TIRPC_CPPFLAGS=""
+	TIRPC_LIBS=""
+
+	AC_CHECK_HEADER(tirpc/netconfig.h,[
+		TIRPC_CPPFLAGS="-I/usr/include/tirpc"
+		AC_DEFINE(HAVE_LIBTIRPC, 1, [Define to 1 if you have libtirpc headers installed])
+		AC_CHECK_LIB(tirpc, rpcb_set, [TIRPC_LIBS="-ltirpc"])])
+
+	AC_SUBST(TIRPC_CPPFLAGS)
+	AC_SUBST(TIRPC_LIBS)
+])

configure.ac
@@ -178,5 +178,6 @@ LTP_CHECK_FCHOWNAT
 LTP_CHECK_MKNODAT
 LTP_CHECK_FALLOCATE
 LTP_CHECK_SYSCALL_FCNTL
+LTP_CHECK_TIRPC
 
 AC_OUTPUT

include/mk/config.mk.default
@@ -42,6 +42,8 @@ YACC			:= bison -y
 #NUMA_CPPFLAGS		:= -DNUMA_VERSION1_COMPATIBILITY
 #NUMA_LIBS		:= -lnuma
 #SELINUX_LIBS		:= -lselinux
+#TIRPC_CPPFLAGS		:= -I/usr/include/tirpc
+#TIRPC_LIBS		:= -ltirpc
 
 prefix			:= /opt/ltp
 

commit 699ed9d3d709b9ac0b2502bf8dcc2e8cf4f56585

tirpc: Detect with pkg-config
Using pkg-config is more reliable way than detecting presence of
libtirpc shared library with headers. As it does not require $SYSROOT,
which is not defined everywhere (at least SDKs built in YOCTO as they
use $SDKTARGETSYSROOT, see [1]).

[1] https://github.com/linux-test-project/ltp/pull/628

Fixes: a86d71fe5 ("Implemented autoconf check for libtirpc")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/lapi/rpc.h
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
+ * Copyright (c) 2018-2020 Petr Vorel <pvorel@suse.cz>
  */
 
 #ifndef LAPI_RPC_H__
@@ -8,20 +8,20 @@
 
 #include "config.h"
 
-#ifdef HAVE_NETCONFIG_H
-# include <netconfig.h>
-# include <rpc/rpc.h>
-# include <rpc/types.h>
-# include <rpc/xdr.h>
-# include <rpc/svc.h>
-#elif defined(HAVE_TIRPC_NETCONFIG_H)
+#ifdef HAVE_LIBTIRPC
 # include <tirpc/netconfig.h>
 # include <tirpc/rpc/rpc.h>
 # include <tirpc/rpc/types.h>
 # include <tirpc/rpc/xdr.h>
 # include <tirpc/rpc/svc.h>
 #else
-# error Missing rpc headers!
+# include <netconfig.h>
+# include <rpc/rpc.h>
+# include <rpc/types.h>
+# include <rpc/xdr.h>
+# include <rpc/svc.h>
+#else
+# error Missing rpc headers! Install libtirpc!
 #endif
 
 #endif	/* LAPI_RPC_H__ */

include/mk/config.mk.in
@@ -43,8 +43,8 @@ CRYPTO_LIBS		:= @CRYPTO_LIBS@
 LEXLIB			:= @LEXLIB@
 NUMA_LIBS		:= @NUMA_LIBS@
 SELINUX_LIBS		:= @SELINUX_LIBS@
-TIRPC_CPPFLAGS		:= @TIRPC_CPPFLAGS@
-TIRPC_LIBS		:= @TIRPC_LIBS@
+LIBTIRPC_CPPFLAGS	:= @LIBTIRPC_CPPFLAGS@
+LIBTIRPC_LIBS		:= @LIBTIRPC_LIBS@
 KEYUTILS_LIBS		:= @KEYUTILS_LIBS@
 HAVE_FTS_H		:= @HAVE_FTS_H@
 

m4/ltp-tirpc.m4
@@ -1,15 +1,8 @@
 dnl SPDX-License-Identifier: GPL-2.0-or-later
-dnl Copyright (c) 2014 Oracle and/or its affiliates. All Rights Reserved.
+dnl Copyright (c) 2019 Petr Vorel <pvorel@suse.cz>
 
-AC_DEFUN([LTP_CHECK_TIRPC],[
-	TIRPC_CPPFLAGS=""
-	TIRPC_LIBS=""
-
-	AC_CHECK_HEADERS([tirpc/netconfig.h netconfig.h], [
-		TIRPC_CPPFLAGS="-I${SYSROOT}/usr/include/tirpc"
-		AC_DEFINE(HAVE_LIBTIRPC, 1, [Define to 1 if you have libtirpc headers installed])
-		AC_CHECK_LIB(tirpc, rpcb_set, [TIRPC_LIBS="-ltirpc"])])
-
-	AC_SUBST(TIRPC_CPPFLAGS)
-	AC_SUBST(TIRPC_LIBS)
+AC_DEFUN([LTP_CHECK_TIRPC], [
+    PKG_CHECK_MODULES([LIBTIRPC], [libtirpc], [
+        AC_DEFINE([HAVE_LIBTIRPC], [1], [Define to 1 if you have libtirpc library and headers])
+	], [have_libtirpc=no])
 ])

travis/debian.cross-compile.aarch64.sh
@@ -2,6 +2,10 @@
 # Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
 set -e
 
+dpkg --add-architecture arm64
+apt update
+
 apt install -y --no-install-recommends \
 	gcc-aarch64-linux-gnu \
-	libc6-dev-arm64-cross
+	libc6-dev-arm64-cross \
+	pkg-config:arm64

travis/debian.cross-compile.ppc64le.sh
@@ -2,6 +2,11 @@
 # Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
 set -e
 
+dpkg --add-architecture ppc64el
+apt update
+
 apt install -y --no-install-recommends \
 	gcc-powerpc64le-linux-gnu \
-	libc6-dev-ppc64el-cross
+	libc6-dev-ppc64el-cross \
+	libtirpc-dev:ppc64el \
+	pkg-config:ppc64el

travis/debian.i386.sh
@@ -16,4 +16,5 @@ apt install -y --no-install-recommends \
 	libkeyutils1:i386 \
 	libnuma1:i386 \
 	libssl-dev:i386 \
-	libtirpc-dev:i386
+	libtirpc-dev:i386 \
+	pkg-config:i386

travis/debian.sh
@@ -35,4 +35,5 @@ apt install -y --no-install-recommends \
 	libssl-dev \
 	libtirpc-dev \
 	linux-libc-dev \
-	lsb-release
+	lsb-release \
+	pkg-config

travis/fedora.sh
@@ -10,4 +10,5 @@ yum -y install \
 	gcc \
 	findutils \
 	libtirpc \
+	pkg-config \
 	redhat-lsb-core

travis/tumbleweed.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
+# Copyright (c) 2018-2019 Petr Vorel <pvorel@suse.cz>
 set -e
 
 zypper --non-interactive install --no-recommends \
@@ -19,4 +19,5 @@ zypper --non-interactive install --no-recommends \
 	libselinux-devel \
 	libtirpc-devel \
 	linux-glibc-devel \
-	lsb-release
+	lsb-release \
+	pkg-config

testcases/network/Makefile
@@ -39,7 +39,7 @@ ifeq ($(ANDROID),1)
 FILTER_OUT_DIRS		+= lib6 rpc sockets
 endif
 
-ifeq ($(TIRPC_LIBS),)
+ifeq ($(LIBTIRPC_LIBS),)
 FILTER_OUT_DIRS		+= rpc
 endif
 