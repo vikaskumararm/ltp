vikaskumararm/ltp

commit eca0fa3c3aa116808cc60d206625c9f8a7bbfa36

tst_device: add new tst_dev_sync
To follow up commit: 0e2ec72de49ab48c151a3c30a1e6ea0cdaea321c
  tst_device: do sync() before reading test block device stat file

Signed-off-by: Li Wang <liwang@redhat.com>
Cc: Cyril Hrubis <chrubis@suse.cz>
Cc: Sumit Garg <sumit.garg@linaro.org>
Cc: Yang Xu <xuyang2018.jy@cn.fujitsu.com>
Acked-by: Cyril Hrubis <chrubis@suse.cz>

include/tst_device.h
@@ -69,6 +69,16 @@ int tst_attach_device(const char *dev_path, const char *file_path);
 int tst_detach_device(const char *dev_path);
 
 /*
+ * To avoid FS deferred IO metadata/cache interference, so we do syncfs
+ * simply before the tst_dev_bytes_written invocation. For easy to use,
+ * we create this inline function tst_dev_sync.
+ */
+static inline void tst_dev_sync(int fd)
+{
+	syncfs(fd);
+}
+
+/*
  * Reads test block device stat file and returns the bytes written since the
  * last call of this function.
  * @dev: test block device

testcases/kernel/syscalls/syncfs/syncfs01.c
@@ -33,8 +33,7 @@ static void verify_syncfs(void)
 
 	fd = SAFE_OPEN(FNAME, O_RDWR|O_CREAT, MODE);
 
-	sync();
-
+	tst_dev_sync(fd);
 	tst_dev_bytes_written(tst_device->dev);
 
 	tst_fill_fd(fd, 0, TST_MB, FILE_SIZE_MB);

testcases/kernel/syscalls/fdatasync/fdatasync03.c
@@ -32,8 +32,7 @@ static void verify_fdatasync(void)
 
 	fd = SAFE_OPEN(FNAME, O_RDWR|O_CREAT, MODE);
 
-	sync();
-
+	tst_dev_sync(fd);
 	tst_dev_bytes_written(tst_device->dev);
 
 	tst_fill_fd(fd, 0, TST_MB, FILE_SIZE_MB);

testcases/kernel/syscalls/fsync/fsync04.c
@@ -32,8 +32,7 @@ static void verify_fsync(void)
 
 	fd = SAFE_OPEN(FNAME, O_RDWR|O_CREAT, MODE);
 
-	sync();
-
+	tst_dev_sync(fd);
 	tst_dev_bytes_written(tst_device->dev);
 
 	tst_fill_fd(fd, 0, TST_MB, FILE_SIZE_MB);

testcases/kernel/syscalls/sync/sync03.c
@@ -32,8 +32,7 @@ static void verify_sync(void)
 
 	fd = SAFE_OPEN(FNAME, O_RDWR|O_CREAT, MODE);
 
-	sync();
-
+	tst_dev_sync(fd);
 	tst_dev_bytes_written(tst_device->dev);
 
 	tst_fill_fd(fd, 0, TST_MB, FILE_SIZE_MB);

testcases/kernel/syscalls/sync_file_range/sync_file_range02.c
@@ -48,8 +48,7 @@ static void verify_sync_file_range(struct testcase *tc)
 
 	lseek(fd, tc->write_off, SEEK_SET);
 
-	sync();
-
+	tst_dev_sync(fd);
 	tst_dev_bytes_written(tst_device->dev);
 
 	tst_fill_fd(fd, 0, TST_MB, tc->write_size_mb);

commit a3e65821af8940f30180d0a9498ce84da9870fd4

syncfs: Fix implicit declaration of function 'syncfs' error
Fixes: eca0fa3c3 tst_device: add new tst_dev_sync

Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/lapi/syncfs.h
@@ -9,6 +9,7 @@
 
 #include "config.h"
 #include <sys/types.h>
+#include <unistd.h>
 #include "lapi/syscalls.h"
 
 #if !defined(HAVE_SYNCFS)

include/tst_device.h
@@ -18,6 +18,8 @@
 #ifndef TST_DEVICE_H__
 #define TST_DEVICE_H__
 
+#include "lapi/syncfs.h"
+
 struct tst_device {
 	const char *dev;
 	const char *fs_type;

testcases/kernel/syscalls/syncfs/check_syncfs.h
@@ -7,6 +7,8 @@
 #ifndef CHECK_SYNCFS_H
 #define CHECK_SYNCFS_H
 
+#include "lapi/syncfs.h"
+
 void check_syncfs(void)
 {
 	int ret;

testcases/kernel/syscalls/syncfs/syncfs01.c
@@ -17,7 +17,6 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include "tst_test.h"
-#include "lapi/syncfs.h"
 #include "check_syncfs.h"
 
 #define MNTPOINT	"mnt_point"