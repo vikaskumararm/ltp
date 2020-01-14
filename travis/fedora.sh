cdffdd2fede2e49e7a1286cf77df66b8f1c156f1
Petr Vorel <pvorel@suse.cz>
2020-01-14 18:01:55 +0100

    DEVEL
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- travis/fedora.sh
+++ travis/fedora.sh
@@ -2,6 +2,10 @@
 # Copyright (c) 2018-2020 Petr Vorel <pvorel@suse.cz>
 set -e
 
+echo "search libmnl"
+yum search libmnl
+echo "-------------"
+
 yum -y install \
 	autoconf \
 	automake \
@@ -9,6 +13,8 @@ yum -y install \
 	clang \
 	gcc \
 	findutils \
-	libmnl-devel \
 	pkg-config \
 	redhat-lsb-core
+
+# CentOS 8 doesn't have libmnl-devel
+yum -y install libmnl-devel || yum -y install libmnl
