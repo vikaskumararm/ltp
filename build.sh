c6882c4f61d426f600c843339e4534ed4c4be9e9
Petr Vorel <pvorel@suse.cz>
2020-02-28 09:39:55 +0100

    build.sh: Add -Werror=missing-prototypes
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@
 
 set -e
 
-CFLAGS="${CFLAGS:--Werror=implicit-function-declaration}"
+CFLAGS="${CFLAGS:--Werror=implicit-function-declaration -Werror=missing-prototypes}"
 CC="${CC:-gcc}"
 
 DEFAULT_PREFIX="$HOME/ltp-install"
